e228ab5325ec02c85e9542591efab56b94a234a5
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-07-09 20:36:07 +0500

    testing file serving with express

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
-// Load the http and fs modules
-var http = require("http");
-var fs = require("fs");
-const testFolder = './static/images';
+// https://expressjs.com/en/resources/middleware/serve-static.html
+import express from "express";
+import fs from "fs";
+import path from "path";
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT | 3000
-// Create an HTTP server that listens for incoming requests on port 8080
-// http.createServer(function(request, response) {
+const app = express();
+const port = process.env.PORT || 3000;
+const imagesFolder = path.join(
+  new URL("./static/images", import.meta.url).pathname,
+  "./"
+);
+// const imgPath = "/static/images/picture3.png";
+// const __dirname = path.resolve();
 
-const server = http.createServer((request, response) => {
-	// If the request is for the root url "/", serve the image file
-	if (request.url == "/") {
-		// Read the image file from the filesystem
-		fs.readdir(testFolder, (err, files) => {
-			var fileName = files[Math.floor(Math.random() * files.length)];
-			console.log(fileName)
-			fs.readFile("./static/images/" + fileName, function (err, data) {
-				if (err) {
-					// If there was an error reading the file, send a 404 Not Found response
-					response.writeHead(404);
-					response.end();
-					return;
-				}
+app.get("/", (req, res) => {
+  // Read the image files from the folder
+  fs.readdir(imagesFolder, (err, files) => {
+    if (err) {
+      // If there was an error reading the folder, send a 500 Internal Server Error response
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    const randomNo = Math.floor(Math.random() * files.length);
+    const fileName = files[randomNo];
+    const filePath = path.join(imagesFolder, fileName);
 
-				// Otherwise, send the image file data in the response
-				response.writeHead(200, {
-					"Content-Type": "image/jpeg",
-					"Cache-Control": "no-cache, no-store, must-revalidate",
-					"Pragma": "no-cache",
-					"Expires": "0"
-				});
-				response.end(data);
+    // Read the image file from the filesystem
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        // If there was an error reading the file, send a 404 Not Found response
+        res.status(404).send("Not Found");
+        return;
+      }
 
-			});
+      // Otherwise, send the image file data in the response
+    //   res.writeHead(200, {
+    //     "Content-Type": "image/jpeg",
+    //     "Cache-Control": "cache",
+    //     Pragma: "no-cache",
+    //     Expires: "0",
+    //   });
+      res.end(data);
+    });
+  });
+});
 
-		})
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
 
-	}
-})
+// import { readdir } from "fs";
+// Read the image file from the filesystem
+// readdir(__dirname, (err, files) => {
+//   console.log("files", files);
+//   var fileName = files[Math.floor(Math.random() * files.length)];
+//   console.log("fileName", fileName);
+// });
 
-
-
-server.listen(port, () => { console.log('Server Started'); });
-// fs.readdir(testFolder, (err, files) => {
-// 	var fileName= files[Math.floor(Math.random()*files.length)];
-// 	console.log(fileName)
-//   });
+// /static/images/picture3.png
+// app.use(express.static('/static/images/picture3.png'))
+// app.get("/", express.static('/static/images/picture3.png'));
